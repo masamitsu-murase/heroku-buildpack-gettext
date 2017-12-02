a2e9e7b8b98b8630d36fc1373506cb9b3e2c40d0
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-12-02 16:03:36 +0900

    Use $HOME.

--- profile/heroku-buildpack-gettext.sh
+++ profile/heroku-buildpack-gettext.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 PATH=$PATH:$HOME/.heroku/gettext/usr/local/bin
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.heroku/gettext/usr/local/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.heroku/gettext/usr/local/lib
